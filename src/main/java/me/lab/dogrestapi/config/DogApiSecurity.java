762de6ec94d071bd1935f2738785ecb4e1af46d2
Yass <yassine.mjardi@gmail.com>
2024-03-17 17:11:17 +0100

    :rocket: Adding security to APIs (Basic auth)

---
+++ src/main/java/me/lab/dogrestapi/config/DogApiSecurity.java
@@ -0,0 +1,53 @@
+package me.lab.dogrestapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class DogApiSecurity  {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/token/**").permitAll()
+                        .anyRequest().authenticated())
+
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+    @Bean
+    public UserDetailsService user() {
+        // The builder will ensure the passwords are encoded before saving in memory
+        UserDetails user = User.builder()
+                .username("admin")
+                .passwordEncoder(passwordEncoder()::encode)
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
